a05373cfb442ccee48f13510bc9c2b440096b4b1
zhouxingrong <zhouxingrong@putao.com>
2017-10-24 15:36:04 +0800

    Initial commit

---
+++ app/src/main/java/com/putao/ptx/qrcode/TotalApplication.java
@@ -0,0 +1,24 @@
+package com.putao.ptx.qrcode;
+
+import android.app.Application;
+
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
+/**
+ * Description:
+ * Copyright  : Copyright (c) 2017
+ * Email      : jusenr@163.com
+ * Author     : Jusenr
+ * Date       : 2017/10/24
+ * Time       : 11:08
+ * Project    ：zxing_qrcode_demo.
+ */
+public class TotalApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ZXingLibrary.initDisplayOpinion(getApplicationContext());
+    }
+}
