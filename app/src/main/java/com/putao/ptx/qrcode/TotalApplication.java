a3a3b2a6692cf18655f100f6385f10f5fea527c7
zhouxingrong <zhouxingrong@putao.com>
2017-11-21 11:52:52 +0800

    fix

--- app/src/main/java/com/putao/ptx/qrcode/TotalApplication.java
+++ app/src/main/java/com/putao/ptx/qrcode/TotalApplication.java
@@ -1,9 +1,11 @@
 package com.putao.ptx.qrcode;
 
 import android.app.Application;
+import android.content.pm.ApplicationInfo;
 import android.support.multidex.MultiDex;
 
 import com.jusenr.toolslibrary.AndroidTools;
+import com.jusenr.toolslibrary.utils.AppUtils;
 import com.umeng.analytics.MobclickAgent;
 
 /**
@@ -28,6 +30,11 @@ public class TotalApplication extends Application {
         //androidtools init
         AndroidTools.init(getApplicationContext(), BuildConfig.LOG_TAG);
         //UMeng init
+        ApplicationInfo info = AppUtils.getApplicationInfo(getApplicationContext());
+        String umeng_appkey = info.metaData.getString("UMENG_APPKEY");
+        String umeng_channel = info.metaData.getString("UMENG_CHANNEL");
+        MobclickAgent.UMAnalyticsConfig config = new MobclickAgent.UMAnalyticsConfig(getApplicationContext(), umeng_appkey, umeng_channel);
+        MobclickAgent.startWithConfigure(config);
         MobclickAgent.setDebugMode(BuildConfig.INNER_TEST);
         MobclickAgent.setCatchUncaughtExceptions(true);
         MobclickAgent.openActivityDurationTrack(false);
