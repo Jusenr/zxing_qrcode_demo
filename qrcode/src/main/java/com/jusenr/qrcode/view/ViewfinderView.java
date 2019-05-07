d68dae0997202d1869492084a4ea69aab7f41bc6
Jusenr <jusenr@163.com>
2019-05-07 14:23:29 +0800

    fixed lang en zh-rCN zh-rTW

--- qrcode/src/main/java/com/jusenr/qrcode/view/ViewfinderView.java
+++ qrcode/src/main/java/com/jusenr/qrcode/view/ViewfinderView.java
@@ -43,11 +43,11 @@ import java.util.HashSet;
  */
 public final class ViewfinderView extends View {
 
-    private static final int LAYER_FLAGS = Canvas.MATRIX_SAVE_FLAG |
-            Canvas.CLIP_SAVE_FLAG |
-            Canvas.HAS_ALPHA_LAYER_SAVE_FLAG |
-            Canvas.FULL_COLOR_LAYER_SAVE_FLAG |
-            Canvas.CLIP_TO_LAYER_SAVE_FLAG;
+//    private static final int LAYER_FLAGS = Canvas.MATRIX_SAVE_FLAG |
+//            Canvas.CLIP_SAVE_FLAG |
+//            Canvas.HAS_ALPHA_LAYER_SAVE_FLAG |
+//            Canvas.FULL_COLOR_LAYER_SAVE_FLAG |
+//            Canvas.CLIP_TO_LAYER_SAVE_FLAG;
 
     private static final long ANIMATION_DELAY = 100L;
     private static final int OPAQUE = 0xFF;
